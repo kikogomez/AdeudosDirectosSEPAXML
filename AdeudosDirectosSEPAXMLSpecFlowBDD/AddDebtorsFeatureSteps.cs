ae08d08fa274d2ed7a6013174ab8de036f10e222
Francisco Gomez-Caldito Viseas <kikogomez@gmail.com>
2015-12-15 18:39:28 +0000

    AdDebtorrFeatureSteps estaba fuera de seguimiento por alguna razon. Se añade

---
+++ AdeudosDirectosSEPAXMLSpecFlowBDD/AddDebtorsFeatureSteps.cs
@@ -0,0 +1,51 @@
+﻿using TechTalk.SpecFlow;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Billing;
+
+namespace AdeudosDirectosSEPAXMLSpecFlowBDD
+{
+    [Binding, Scope(Feature = "Add Debtors")]
+    public class AddDebtorsFeatureSteps
+    {
+        private readonly DebtorsManagementContextData debtorsManagementContextData;
+
+        public AddDebtorsFeatureSteps(DebtorsManagementContextData debtorsManagementContextData)
+        {
+            this.debtorsManagementContextData = debtorsManagementContextData;
+        }
+
+        [Given(@"These names ""(.*)"", ""(.*)"", ""(.*)""")]
+        public void GivenTheseNames(string givenName, string firstSurname, string secondSurname)
+        {
+            debtorsManagementContextData.givenName = givenName;
+            debtorsManagementContextData.firstSurname = firstSurname;
+            debtorsManagementContextData.secondSurname = secondSurname;
+        }
+
+        [When(@"I process the names")]
+        public void WhenIProcessTheNames()
+        {
+            Debtor debtor;
+            try
+            {
+                debtor = new Debtor(
+                    "0001",
+                    debtorsManagementContextData.givenName,
+                    debtorsManagementContextData.firstSurname,
+                    debtorsManagementContextData.secondSurname);
+                debtorsManagementContextData.debtor = debtor; ;
+            }
+            catch
+            {
+                debtorsManagementContextData.debtor = null;
+            }     
+        }
+
+        [Then(@"The name is considered ""(.*)""")]
+        public void ThenTheNameIsConsidered(string validity)
+        {
+            bool valid = (validity == "valid" ? true : false);
+            Assert.AreEqual(debtorsManagementContextData.debtor != null, valid);
+        }
+    }
+}
