63cef86f5b0c388129155952f7a15eed65be54e3
Francisco Gomez-Caldito Viseas <kikogomez@gmail.com>
2016-02-02 14:39:15 +0000

    Creando una aplicacion basica de consola

---
+++ SEPAXMLCustomerDirectDebitInitiationGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace SEPAXMLCustomerDirectDebitInitiationGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDataTable = new DataTable();
+            using (var conection = new OleDbConnection("Provider=Microsoft.JET.OLEDB.4.0;" + "data source=C:\\menus\\newmenus\\menu.mdb;Password=****"))
+            {
+                conection.Open();
+                var query = "Select siteid From n_user";
+                var command = new OleDbCommand(query, conection);
+                var reader = command.ExecuteReader();
+            }
+
+            using (var conection = new OleDbConnection("Provider=Microsoft.JET.OLEDB.4.0;" + "data source=C:\\menus\\newmenus\\menu.mdb;Password=****"))
+            {
+                conection.Open();
+                var query = "Select siteid From n_user";
+                var adapter = new OleDbDataAdapter(query, conection);
+                adapter.Fill(myDataTable);
+                string text = myDataTable.Rows[0][0].ToString();
+            }
+        }
+    }
+}
