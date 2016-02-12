196962138abb5e9fe8609d0c344e4c0ce412a30f
Francisco Gomez-Caldito Viseas <kikogomez@gmail.com>
2016-02-12 18:07:11 +0000

    Preparando el programa de lectura de devoluciones

---
+++ SEPAXMLPaymentStatusReader/ArgumentOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommandLine;
+using CommandLine.Text;
+using System.Threading.Tasks;
+
+namespace SEPAXMLPaymentStatusReader
+{
+   public class ArgumentOptions
+    {
+        [Option('i', "inputPaymentStatusReportFullPath", Required = true, HelpText = "Payment Status Report full path.")]
+        public string SourceDataBase { get; set; }
+
+        [Option('o', "outputDataBaseFullPath", Required = true, HelpText = "Output Data Base Full Path.")]
+        public string OutputXMLFile { get; set; }
+
+        [Option('v', null, HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            // this without using CommandLine.Text
+            var usage = new StringBuilder();
+            usage.AppendLine("SEPA XML Payment Status Report (pain.002.001.03) Reader");
+            usage.AppendLine("Arguments error.");
+            usage.AppendLine("Required arguments:");
+            usage.AppendLine("   -i InputPaymentStatusReportFullPath");
+            usage.AppendLine("   -o OutputDataBaseFullPath");
+            return usage.ToString();
+        }
+    }
+}
---
+++ SEPAXMLPaymentStatusReader/ExitCodes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPAXMLPaymentStatusReader
+{
+    public enum ExitCodes
+    {
+
+        /// Normal Exit
+        Success = 0,
+
+        /// Invalid command line options
+        InvalidArguments = -1,
+
+        /// DataBase Not Found
+        InvalidDataBasePath = -2,
+
+        /// Source data file not found
+        InvalidPaymentStatusFilePath = -3,
+
+        /// DataBase connection error
+        DataBaseConnectionError = -4,
+
+        /// File Reading Error
+        FileReadingError = -5,
+
+        /// Data Reading error
+        DataBaseWritingError = -6,
+    }
+}
